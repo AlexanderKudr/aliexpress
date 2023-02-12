024152f4cbcaa69d709b7f7a5ace979a519f044d
Alex <trevan98@gmail.com>
2023-02-12 18:48:28 +0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
 
 function App() {
+  const [scroll, setScroll] = useState(0)
+  const yourfunction = () => {
+    const removeOnScroll = window.scrollY > 200;
+    //stuff with classes
+   
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", yourfunction);
+    console.log(window.scrollY)
+    return () => {
+      window.removeEventListener("scroll", yourfunction);
+    };
+  }, []);
+
   return <div className="App">dev test again</div>;
 }
 
