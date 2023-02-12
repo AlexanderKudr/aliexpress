c1a4b55a7ef8a31c2cf31d321790954dace606c5
Alex <trevan98@gmail.com>
2023-02-12 20:01:55 +0300

    added initials

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,31 @@
-import { createBrowserRouter} from "react-router-dom";
-import Root from "../pages/Root"
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../pages/Root";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>,
+    element: <Root />,
+    children: [
+      {
+        path: "/category/:id",
+        element: <Root />, //todo: add category from pages
+      },
+      {
+        path: "/product/:id",
+        element: <Root />, //todo: add product from pages
+      },
+    ],
+  },
+  {
+    path: "/user/signup",
+    element: <Root />, //todo: add signup from pages
+  },
+  {
+    path: "/user/login",
+    element: <Root />, //todo: add login from pages
+  },
+  {
+    path: "/dashboard",
+    element: <Root />, //todo: add dashboard from pages
   },
 ]);
