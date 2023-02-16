85d5b7f9b72584199b020bc038a74f5908f6ad97
thatonerookiedev <110988678+Thatonerookiedev@users.noreply.github.com>
2023-02-16 12:37:49 -0600

    last commint hopefully

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { Icons } from "../components/icons";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef,useState } from "react";
 import img1 from '../assets/images/signup-pic-1-2.jpg'
 import img2 from '../assets/images/sigup-picture-1-1.jpg'
 import img3 from '../assets/images/signup-pic-1-3.jpg'
@@ -11,13 +11,36 @@ import img7 from '../assets/images/car1.jpg'
 import img8 from '../assets/images/car2.jpg'
 import img9 from '../assets/images/car3.jpg'
 
-const SignUpPage = () => {
-//-----------------------------
 
+
+
+
+
+const SignUpPage = () => {
     const card1:any = useRef()
     const card2:any = useRef()
     const card3:any = useRef()
-    
+    let radio1:any = useRef()
+    let radio2:any = useRef()
+    let radio3:any = useRef()
+    let previousnumber:number = 0
+    let loop = 1
+    const [radio1n2,updateradio1] = useState<any>(0)
+    const [radio2n2,updateradio2] = useState<any>(0)
+    const [radio3n2,updateradio3] = useState<any>(0)
+
+    useEffect(()=>{
+        if(radio1.current){
+            updateradio1(radio1.current)
+        }
+        if(radio2.current){
+            updateradio2(radio2.current)
+        }
+        if(radio3.current){
+            updateradio3(radio3.current)
+        }
+    },[])
+
     function ButtonsignUp(e:any){
 
         
@@ -32,6 +55,7 @@ const SignUpPage = () => {
         card1main.style.backgroundImage = `url(${img1})`;
         card2main.style.backgroundImage = `url(${img2})`;
         card3main.style.backgroundImage = `url(${img3})`;
+        return loop = 2
        }else{
 
        }
@@ -40,6 +64,7 @@ const SignUpPage = () => {
         card1main.style.backgroundImage = `url(${img4})`;
         card2main.style.backgroundImage = `url(${img5})`;
         card3main.style.backgroundImage = `url(${img6})`;
+        return loop = 3
        }else{
 
        }
@@ -48,165 +73,177 @@ const SignUpPage = () => {
         card1main.style.backgroundImage = `url(${img7})`;
         card2main.style.backgroundImage = `url(${img8})`;
         card3main.style.backgroundImage = `url(${img9})`;
+        return loop = 1
        }else{
 
        }
         
 
     }
-
-
-    let radio1 = useRef()
-    let radio2 = useRef()
-    let radio3 = useRef()
-
-
-    let previousnumber:number = 0
-
-    function arrowSwitch(e:any){
+    
+    function arrowSwitch(type:string){
         let card1main:any = card1.current
         let card2main:any = card2.current
         let card3main:any = card3.current
 
-        let radio1main = radio1.current
-        let radio2main = radio2.current
-        let radio3main = radio3.current
+        let radio1main = radio1n2
+        let radio2main = radio2n2
+        let radio3main = radio3n2
 
 
+        if(radio1main && radio2main && radio3main !== 0){
+            if(loop<4){
+                
+                if(loop === 1){
+                    setTimeout(()=>{
+                    
+                    card1main.style.backgroundImage = `url("${img1}")`
+                    card2main.style.backgroundImage = `url("${img2}")`
+                    card3main.style.backgroundImage = `url("${img3}")`
+                    radio1main.checked = true
+                   
 
-        let currentnumber:number = previousnumber + 1
-        let radiocheck = currentnumber
+                    
+                        if(type == 'increase'){
+                            return loop = 2
+                        }else{
+                            return loop = 3
+                        }
 
-        if(previousnumber < 3){
-            previousnumber = currentnumber
-        }else{
-            radio2main.checked = true;
-            radio2main.click()
-            previousnumber = 0
-        }
+                    },1000)
 
-        if(radiocheck == 1){
-            radio2main.checked = true;
-            radio2main.click()
-           
-        }
+                   
+                }
 
-        if(radiocheck == 2){
-            radio3main.checked = true;
-            radio3main.click()
-           
-        }
+                if(loop === 2){
+                    setTimeout(()=>{
+                    
+                    card1main.style.backgroundImage = `url("${img4}")`
+                    card2main.style.backgroundImage = `url("${img5}")`
+                    card3main.style.backgroundImage = `url("${img6}")`
+                    radio2main.checked = true
+                 
+                   
+                   
+                        if(type === 'increase'){
+                            return loop = 3
+                        }
+                        if(type === 'decrease'){
+                            return loop = 1
+                        }
+                    },1000)
+                }
 
-        if(radiocheck == 3){
-            radio1main.checked = true;
-            radio1main.click()
-           
+                if(loop === 3){
+                    setTimeout(()=>{
+                    
+                    card1main.style.backgroundImage = `url("${img7}")`
+                    card2main.style.backgroundImage = `url("${img8}")`
+                    card3main.style.backgroundImage = `url("${img9}")`
+                    radio3main.checked = true
+                   
+                   
+                    
+                        if(type === 'increase'){
+                            return loop = 1
+                        }
+                        if(type === 'decrease'){
+                            return loop = 2
+                        }
+                    },1000)
+                }
+
+            }
         }
 
 
     }
-
-    let prevousloop:number = 2
-
     
       setInterval(()=>{
-        
-            if(prevousloop>0){
-    
-                setTimeout((i:number)=>{
-                    console.log('this is i', prevousloop)
-        
-                    if(prevousloop == 2){
-                        prevousloop = 2
-                    }
-        
-        
-                    arrowSwitch(radio1.current)
-                    return
-                },5000)
-        
-            }
+        console.log('i ran')
+                
+        arrowSwitch('increase')
           
-      },2000)
+      },5000)
         
-    
-
-    
 
 
 
+const Right = () =>{
+    return (
+        <>
+        <div className="rightcontainer">
+            <span id="span1">
+            <div className="card" ref={card1} id="card-1"></div>
+            <div className="card 2" ref={card2} id="card-2"></div>
+            <div className="card" ref={card3} id="card-3"></div>
+            </span>
+            <h6 id="h61">Lorem ipsum</h6>
+            <p id="p1">Get new fashioned clothes with 50% sale 
+            every weekend</p>
+            <span id="span2">
+            <button className="arrows-signup2" onClick={(e)=>{arrowSwitch('decrease')}} id="decrease"></button>
+                
+                    <label className="b-contain">
+                        <input name="radio" className="none" id="1" type="radio" ref={radio1} onClick={(e)=>{ButtonsignUp(e)}} />
+                        <div className="b-input" ></div>
+                    </label>
+                    <label className="b-contain">
+                        <input name="radio" className="none" id="2" type="radio" ref={radio2} onClick={(e)=>{ButtonsignUp(e)}} />
+                        <div className="b-input" ></div>
+                    </label>
+                    <label className="b-contain">
+                        <input name="radio" className="none" id="3" type="radio" ref={radio3} onClick={(e)=>{ButtonsignUp(e)}} />
+                        <div className="b-input"></div>
+                    </label>
 
+       
+                   <button className="arrows-signup2 two" onClick={()=>{arrowSwitch('increase')}} id="inecrease"></button>       
+            </span>
+        </div>
+        </>
+    )
+}
+
+const Left = () =>{
+    return(
+        <>
+        <div className="left">
+            <div className="contanierleft"> 
+            <span className="span9"><Icons.Logo/></span>
+            <h6 id="signUp">Sign up</h6>
+            <h6 id="sign-mini">Welcome, please enter your details</h6>
+            {/*  */}
+            <h6 className="headers-sign">First name</h6>
+            <input type="text" name="" placeholder="Enter your firstname" id="" className="input-sign" />
+            <h6 className="headers-sign">Last name</h6>
+            <input type="text" placeholder="Enter your lastname" name="" id="" className="input-sign" />
+            <h6 className="headers-sign">Email</h6>
+            <input type="text" name="" placeholder="Enter your email" id="" className="input-sign" />
+            <h6 className="headers-sign">Password</h6>
+            <input type="text" name="" placeholder="Enter your password" id="" className="input-sign" />
+            <button className="signup-multicolor">Sign up</button>
+            <button className="signup-Google"><span id="signupgoogle-1"><div><Icons.Google/></div> Sign up with Google</span></button>
+            <p id="bottom-par">Already have signed up?  <span id="bottom-span"><Link to=''>Click here </Link></span></p>
+            </div>
+        </div>
+        </>
+    )
+}
 
 
 
     return ( 
         <>
-        <div id="border-picture">
-             {/* -------------------- */}
-             <div id="flex-signup-1">
-            <div className="card" ref={card1} id="card-1"></div>
-            <div className="card 2" ref={card2} id="card-2"></div>
-            <div className="card" ref={card3} id="card-3"></div>
-            </div>
-            {/* -------------------- */}
-            <h6 id="h6">Lorem ipsum</h6>
-            <p id="p">Get new fashioned clothes with 50% sale 
-            every weekend</p>
-            <div className="carosell-containner">
-                {/* ------------------------ */}
-                <button className="arrows-signup" onClick={(e)=>{arrowSwitch(e)}} id="decrease"></button>
-                       
-                <label className="control control-radio">
-                        <input type="radio" id="1" ref={radio1} onClick={(e)=>{ButtonsignUp(e)}} className="img-butt-signup" name="imges-signup" />
-                    <div className="control_indicator"></div>
-                </label> 
-
-                <label className="control control-radio">
-                        <input type="radio" id="2" ref={radio2} onClick={(e)=>{ButtonsignUp(e)}} className="img-butt-signup" name="imges-signup" />
-                    <div className="control_indicator"></div>
-                </label> 
-
-                <label className="control control-radio">
-                        <input type="radio" id="3" ref={radio3} onClick={(e)=>{ButtonsignUp(e)}} className="img-butt-signup" name="imges-signup" />
-                    <div className="control_indicator"></div>
-                </label> 
-
-            <button className="arrows-signup" onClick={(e)=>{arrowSwitch(e)}} id="inecrease"></button>
-
-            </div>
-        </div>
-        {/* --------------------------------- */}
-        <div className="signup-container">
-            <div className="container-signup-left t">
-                <div>
-                <Icons.Logo/>
-                <h6 id="h62">Sign Up</h6>
-                <h6 id="small">Welcome, please enter your details</h6>
-                </div>
-                <span id="span">
-                <h6 className="labels-signup pad">First Name</h6>
-                <input type="text" className="pad pad-input" placeholder="Enter your first name" name="" id="1" />
-                <h6 className="labels-signup pad">Last Name</h6>
-                <input type="text" className="pad pad-input" placeholder="Enter your last name" name="" id="2" />
-                <h6 className="labels-signup pad">Email</h6>
-                <input type="text" className="pad pad-input" placeholder="Enter your email" name="" id="" />
-                <h6 className="labels-signup pad">Password</h6>
-                <input type="text" className="pad pad-input" placeholder="Enter your password" name="" id="3" />
-                </span>
-                {/* --------------------- */}
-                <span className="buttons-and-errordiv-signup">
-                    <div className="error-signup"></div>
-                    <button className="signup">Sign Up</button>
-                    <br />
-                    <button className="google-signup"><span id="Google-icon-Signup"><Icons.Google/></span> <p> Sign Up with Google</p></button>
-                    <p className="already">Already Signed Up? <Link to='/user/login'>Click Here!</Link></p>
-                </span>
-            </div>
-        </div>
+        <Left/>
+        <Right/>
         </>
+        
      );
 }
  
 export default SignUpPage;
+
+
 
 
