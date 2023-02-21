f49188d467ca038e0d30242600e239d2535b1db2
thatonerookiedev <110988678+Thatonerookiedev@users.noreply.github.com>
2023-02-20 18:25:44 -0600

    carosel 1 done ecept for mini styles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,38 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import img11 from '../assets/images/homepage-before-one.png'
+import img12 from '../assets/images/img12-homepage1.jpg'
+import img13 from '../assets/images/img13-homepage1.jpg'
+import img14 from '../assets/images/backgroundimg-homepage1-14.jpg'
 
 
 const Homepage = () => {
-    const [button1color,updatdebutton1color] = useState<string>('yellow')
-    const [button2color,updatdebutton2color] = useState<string>('grey')
-    const [button3color,updatdebutton3color] = useState<string>('grey')
-    const [button4color,updatdebutton4color] = useState<string>('grey')
+    const [button1color,updatdebutton1color] = useState<string>('#FFBF85')
+    const [button2color,updatdebutton2color] = useState<string>('#D1D1D1')
+    const [button3color,updatdebutton3color] = useState<string>('#D1D1D1')
+    const [button4color,updatdebutton4color] = useState<string>('#D1D1D1')
     // -------------------
     const [loop1,updateloop1] = useState<number>(1)
+    let [backgroundimg1,updatebackgroundimg1]= useState<any>(img11)
+    let [intervalloop1,updateintervalloop1] = useState<number>(0)
     
 
     function updateobject(ids:Array<number>){
        
         ids.map((item)=>{
             if(item === 11){
-                updatdebutton1color('grey')
+                updatdebutton1color('#D1D1D1')
                 return
             }
             if(item === 12){
-                updatdebutton2color('grey')
+                updatdebutton2color('#D1D1D1')
                 return
             }
             if(item === 13){
-                updatdebutton3color('grey')
+                updatdebutton3color('#D1D1D1')
                 return
             }
             if(item === 14){
-                updatdebutton4color('grey')
+                updatdebutton4color('#D1D1D1')
                 return
             }
         })
@@ -44,21 +50,27 @@ const Homepage = () => {
     let idnumbers = numberarray.filter((item)=>{
         return item === target
     })
+    let tempnumber = Math.abs(target - 10)
+    console.log('this is temp number',tempnumber)
 
     updateobject(notidnumbers)
 
     idnumbers.forEach((item)=>{
         if(item === 11){
-            updatdebutton1color('yellow')
+            updatdebutton1color('#FFBF85')
+            updatebackgroundimg1(img11)
         }
         if(item === 12){
-            updatdebutton2color('yellow')
+            updatdebutton2color('#FFBF85')
+            updatebackgroundimg1(img12)
         }
         if(item === 13){
-            updatdebutton3color('yellow')
+            updatdebutton3color('#FFBF85')
+            updatebackgroundimg1(img13)
         }
         if(item === 14){
-            updatdebutton4color('yellow')
+            updatdebutton4color('#FFBF85')
+            updatebackgroundimg1(img14)
         }
     })
 
@@ -68,12 +80,17 @@ const Homepage = () => {
         if(crement === 'increment' && loop1 < 4){
             const number1 = loop1 +1
            updateloop1(number1)
-           
+           if(loop1 == 4){
+            updateloop1(1)
+           }
         }
 
         if(crement === 'decrement' && loop1 > 1){
             const number1 = loop1 - 1
             updateloop1(number1)
+            if(loop1 == 1){
+                updateloop1(4)
+               }
         }
         let number = loop1 + 10
         console.log(loop1,'loop1',number,'number')
@@ -82,10 +99,65 @@ const Homepage = () => {
        
     }
 
+  
+    
+
+    // setInterval(()=>{
+    //     updateintervalloop1(1)
+    //     if(intervalloop1 !== 1 || 2 || 3){
+    //             if(loop1 === 4){
+    //                 console.log('it is 4')
+    //                 updatetempvar('decrement')
+    //             }
+    //             if(loop1 === 1){
+    //                 console.log('it is 4')
+    //                 updatetempvar('increment')
+    //             }
+    //             console.log('it is going this way',tempvar)
+    //                 arrows1(tempvar)
+            
+                
+      
+    //     }
+    // },5000)
+
+    // useEffect(()=>{
+    //     setTimeout(()=>{
+    //         if(loop1 === 4){
+    //             console.log('it is 4')
+    //             updatetempvar('decrement')
+    //         }
+    //         if(loop1 === 1){
+    //             console.log('it is 4')
+    //             updatetempvar('increment')
+    //         }
+    //         console.log('hi')
+    //     },2000)
+    // },[backgroundimg1])
+
+    const [tempvar,updatetempvar] = useState('increment')
+
+    function Interval(){
+        setTimeout(()=>{
+           updateintervalloop1(intervalloop1+1)
+           if(loop1 == 4){
+            updatetempvar('decrement')
+           }
+           if(loop1 == 1){
+            updatetempvar('increment')
+           }
+           arrows1(tempvar)
+           console.log('loop',intervalloop1)
+         },2000)
+    }
+     useEffect(()=>Interval(),[intervalloop1])
+
+
+
 const Top = ()=>{
     return(
         <>
-        <div className="big-container-one">
+        <div style={{backgroundImage: `url(${backgroundimg1})`}} className="big-container-one">
            <h1 id="topHompageone">Build your dream decoration</h1>
            <h6 id="topHompagetwo">Lorem ipsum is a placehloder that is used by developers</h6>
            {/* ------------------ */}
@@ -94,13 +166,13 @@ const Top = ()=>{
             <button className="loop" onClick={()=>{arrows1('increment')}}></button>
            </div>
            <div className="button-flex-div1">
-            <button id="11" style={{width:'1.2rem',height:'1.2rem',border:'1px solid black',borderRadius:'50%',cursor:'pointer',backgroundColor:button1color}} onClick={()=>{buttons(11)}}></button>
+            <button id="11" style={{width:'1.2rem',height:'1.2rem',border:'0',borderRadius:'50%',cursor:'pointer',backgroundColor:button1color}} onClick={()=>{buttons(11)}}></button>
 
-            <button id="12" style={{width:'1.2rem',height:'1.2rem',border:'1px solid black',borderRadius:'50%',cursor:'pointer',backgroundColor:button2color}} onClick={()=>{buttons(12)}}></button>
+            <button id="12" style={{width:'1.2rem',height:'1.2rem',border:'0',borderRadius:'50%',cursor:'pointer',backgroundColor:button2color}} onClick={()=>{buttons(12)}}></button>
 
-            <button id="13" style={{width:'1.2rem',height:'1.2rem',border:'1px solid black',borderRadius:'50%',cursor:'pointer',backgroundColor:button3color}} onClick={()=>{buttons(13)}}></button>
+            <button id="13" style={{width:'1.2rem',height:'1.2rem',border:'0',borderRadius:'50%',cursor:'pointer',backgroundColor:button3color}} onClick={()=>{buttons(13)}}></button>
 
-            <button id="14" style={{width:'1.2rem',height:'1.2rem',border:'1px solid black',borderRadius:'50%',cursor:'pointer',backgroundColor:button4color}} onClick={()=>{buttons(14)}}></button>
+            <button id="14" style={{width:'1.2rem',height:'1.2rem',border:'0',borderRadius:'50%',cursor:'pointer',backgroundColor:button4color}} onClick={()=>{buttons(14)}}></button>
            </div>
         </div>
         </>
