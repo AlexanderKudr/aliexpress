dec6112df0e3a37a6cae8575e3ce30dd1b6f42a4
thatonerookiedev <110988678+Thatonerookiedev@users.noreply.github.com>
2023-02-23 11:27:33 -0600

    not done

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,6 +11,15 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import apple from '../assets/images/outline-apple.png'
 import laptop from '../assets/images/laptop-home-1.png'
+import Shoe from '../assets/images/shoeheel.png'
+import coffeegroup from '../assets/images/coffeegroup.png'
+import bearCar from '../assets/images/bearandcar.png'
+import notebook from '../assets/images/notebooklaptop.png'
+import money from '../assets/images/$4200.png'
+import hearticon from '../assets/images/hearticon.png'
+import shoppingcartred from '../assets/images/shoppingcartred.png'
+import star from '../assets/images/starticon.png'
+
 
 const Newtop = ()=>{
     return(
@@ -78,6 +87,10 @@ const Popularcatagory = ()=>{
                     <button className="goto">
                         <img src={arrow} alt="" />
                     </button>
+                    <p className="shoe-par">10% off 
+                    <br />
+                    <span className='span-shoe'>The most fashoined shoes with low price</span></p>
+                    <img src={Shoe} alt="" className="shoe" />
                 </div>
 
                 <div className="flex-con-home-2">
@@ -86,20 +99,57 @@ const Popularcatagory = ()=>{
                     </button>
                     <img src={apple} alt="" className="apple" />
                     <img src={laptop} alt="" className='laptop' />
+                    <p className='paragraph-lap'><span className="percent-home"> 5% off</span>
+                    <br />
+                     Every sort of laptops based on your requirements</p>
                 </div>
 
                 <div className="flex-con-home-2">
                     <button className="goto">
                         <img src={arrow} alt="" />
                     </button>
+                    <img src={coffeegroup} alt="" className="coffegroup" />
+                    <p className="coffeepar">Drink the most well known and delicious coffee </p>
                 </div>
 
                 <div className="flex-con-home-2">
                     <button className="goto">
                         <img src={arrow} alt="" />
                     </button>
+                    <img src={bearCar} alt="" className="carandbear" />
+                    <p className="carandbearp"><span className="percent-home">15% off</span>
+                    <br />
+                     Every type of toys that make your children happy</p>
                 </div>
 
+            </div>
+        </div>
+        </>
+    )
+}
+
+const Product = ()=>{
+    return(
+        <>
+        <div className="product-con">
+            <div className="picture-div-home">
+                <img src={notebook} alt="" className="img-con-home" />
+                <img src={hearticon} alt="" className="hearticon" />
+            </div>
+            <div className="numberheading">
+                <h6 className="h6products">$3500</h6>
+                <img src={money} alt="" className="money" />
+            </div>
+            <div className="paragraph-home-prod">
+                <p>Apple macbook pro 512 GB ssd ram 64 GB...</p>
+            </div>
+            <div className="buttons-home">
+                <button className='button-bottom-1'>
+                    <img src={star} alt="" className="star" />
+                    <p className="fifty">50</p>
+                </button>
+                <p className="p-product-home">Sold 100</p>
+                <img src={shoppingcartred} alt="" className="redshopping" />
             </div>
         </div>
         </>
@@ -109,15 +159,46 @@ const Popularcatagory = ()=>{
 const ProductsHome = ()=>{
     return(
         <>
-        <div className="pop-container-1">
-            <h1 className='pop-1'>Popular Products</h1>
-            <div className="flex2-1">
-                <div className="flex-con-home-2-2"></div>
-                <div className="flex-con-home-2-2"></div>
-                <div className="flex-con-home-2-2"></div>
-                <div className="flex-con-home-2-2"></div>
-            </div>
+        <div className="products-con-2">
+            <Swiper
+               modules={[Navigation, Pagination, Scrollbar, A11y]}
+             spaceBetween={0}
+             pagination={{ clickable: true }}
+             slidesPerView={5}
+             onSlideChange={() => console.log('slide change')}
+             onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <Product/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Product/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Product/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Product/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Product/>
+                </SwiperSlide>
+                {/* -------- */}
+                <SwiperSlide>
+                    <Product/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Product/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Product/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Product/>
+                </SwiperSlide>
+            </Swiper>
         </div>
+        <br />
         </>
     )
 }
@@ -127,12 +208,13 @@ const Homepage = () => {
 
     return ( 
         <>
-        <div className="big-one">
+       
       <Newtop/>
       <div className="container-11">
       <Popularcatagory/>
       </div>
-      </div>
+      <ProductsHome/>
+ 
         </>
      );
 }
